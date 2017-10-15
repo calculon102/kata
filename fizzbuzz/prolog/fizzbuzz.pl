dc5bfb22229fa8c9cfd34b85201bf52fe956a871
Frank Großgasteiger <frank@pixelgerecht.de>
2017-10-15 15:22:55 +0200

    Optimized FizzBuzz-solution in Prolog.

--- fizzbuzz/prolog/fizzbuzz.pl
+++ fizzbuzz/prolog/fizzbuzz.pl
@@ -1,39 +1,20 @@
-% FizzBuzz-Kata made in Prolog. Just for fun.
-
+/* FizzBuzz-Kata made in Prolog. Just for fun. */
 
 % Basic rules.
-fizz(X) :- X mod 3 =:= 0.
-buzz(X) :- X mod 5 =:= 0.
+is_fizz(X) :- X mod 3 =:= 0.
+is_buzz(X) :- X mod 5 =:= 0.
+is_fizzbuzz(X) :- is_fizz(X), is_buzz(X).
 
+% FizzBuzz-relations.
+fizzbuzz(X, 'FizzBuzz') :- is_fizzbuzz(X), !.
+fizzbuzz(X, 'Fizz') :- is_fizz(X), !.
+fizzbuzz(X, 'Buzz') :- is_buzz(X), !.
+fizzbuzz(X, X).
 
-% Ordered testing of business-rules with output.
-fizzbuzz_element(X) :- fizz(X), buzz(X), write('FizzBuzz').
-fizzbuzz_element(X) :- fizz(X), write('Fizz').
-fizzbuzz_element(X) :- buzz(X), write('Buzz').
-fizzbuzz_element(X) :- write(X).
+% Apply FizzBuzz on a list.
+fizzbuzz_list([],[]).
+fizzbuzz_list([I|Is], [O|Os]) :- fizzbuzz(I, O), fizzbuzz_list(Is, Os).
 
-
-% Solution giving a list as single parameter.
-%
-% Example:
-% ?- fizzbuzz_list([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]).
-%
-fizzbuzz_list([]).
-fizzbuzz_list([H|T]) :-
-  fizzbuzz_element(H), 
-  write('\n'),
-  fizzbuzz_list(T).
-
-
-% Solution giving a range from X to Y
-%
-% Example:
-% ?- fizzbuzz_range(1,100).
-%
-fizzbuzz_range(X, Y) :- X > Y.
-fizzbuzz_range(X, Y) :- 
-  fizzbuzz_element(X),
-  write('\n'),
-  X1 is X+1, 
-  fizzbuzz_range(X1, Y).
+% Example-Usage: 
+% ?- findall(I, between(1, 100, I), Is), fizzbuzz_list(Is, Os), print(Os).
 
